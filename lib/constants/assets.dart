bc6d7adc273d94458778842789d771be9423dfae
titicodes <anietimfoneeffiong@gmail.com>
2023-08-15 15:46:11 +0100

    first commit

---
+++ lib/constants/assets.dart
@@ -0,0 +1,11 @@
+abstract class AssetsValues {
+  static const addressImage = "assets/images/address_image.png";
+  static const paymentError = "assets/images/payment_error.png";
+  static const paymentSuccess = "assets/images/payment_success.png";
+  static const secondScreenBackground =
+      "assets/images/secondscreen_background.jpg";
+  static const splashScreen = 'assets/images/splash_screen.jpg';
+}
+
+abstract class SvgValues{
+  }
