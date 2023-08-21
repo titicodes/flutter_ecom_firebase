432b0a5610b8550d8e3f4adb024b2d1c5c2c9cc9
titicodes <anietimfoneeffiong@gmail.com>
2023-08-21 11:41:53 +0100

    designing a longin_view

--- lib/modules/auth/views/login_register.dart
+++ lib/modules/auth/views/login_register.dart
@@ -1,89 +1,103 @@
 import 'package:flutter/material.dart';
 
+import 'login_view.dart';
+
 class LoginRegister extends StatelessWidget {
   const LoginRegister({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      physics: const ScrollPhysics(),
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 60.0,
-          ),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+    return Scaffold(
+        appBar: AppBar(),
+        body: SingleChildScrollView(
+          physics: const ScrollPhysics(),
+          child: Column(
             children: [
-              Icon(
-                Icons.map_outlined,
-                color: Colors.black,
+              const SizedBox(
+                height: 60.0,
               ),
-              Text(
-                'Keliene',
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.map_outlined,
+                    color: Colors.black,
+                  ),
+                  Text(
+                    'Keliene',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'Poppins',
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 40.0),
+              const Text(
+                'The Right Address \n for shopping \n Anyday',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Poppins',
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              const Text(
+                'It is now very easy to reach \nto reach the best quality among all',
                 style: TextStyle(
                     color: Colors.black,
                     fontFamily: 'Poppins',
                     fontSize: 14,
-                    fontWeight: FontWeight.w400),
+                    fontWeight: FontWeight.w300),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 40.0),
+              Container(
+                alignment: Alignment.center,
+                margin: const EdgeInsets.symmetric(horizontal: 40),
+                height: 50,
+                width: double.infinity,
+                decoration:
+                    const BoxDecoration(color: Color.fromARGB(255, 9, 86, 150)),
+                child: const Text(
+                  'Register',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400),
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              GestureDetector(
+                // Within the `FirstRoute` widget
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginView()),
+                  );
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  margin: const EdgeInsets.symmetric(horizontal: 40),
+                  height: 50,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 232, 235, 238)),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400),
+                  ),
+                ),
               ),
             ],
           ),
-          const SizedBox(height: 40.0),
-          const Text(
-            'The Right Address \n for shopping \n Anyday',
-            style: TextStyle(
-                color: Colors.black,
-                fontFamily: 'Poppins',
-                fontSize: 20,
-                fontWeight: FontWeight.w700),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(
-            height: 30.0,
-          ),
-          const Text(
-            'It is now very easy to reach \nto reach the best quality among all',
-            style: TextStyle(
-                color: Colors.black,
-                fontFamily: 'Poppins',
-                fontSize: 14,
-                fontWeight: FontWeight.w300),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 40.0),
-          Container(
-            alignment: Alignment.center,
-            margin: const EdgeInsets.symmetric(horizontal: 40),
-            height: 50,
-            width: double.infinity,
-            decoration: const BoxDecoration(color: Color.fromARGB(255, 9, 86, 150)),
-            child: const Text(
-              'Register',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400),
-            ),
-          ),
-
-          const SizedBox(height: 20.0),
-          Container(
-            alignment: Alignment.center,
-            margin: const EdgeInsets.symmetric(horizontal: 40),
-            height: 50,
-            width: double.infinity,
-            decoration: const BoxDecoration(color: Color.fromARGB(255, 232, 235, 238)),
-            child: const Text(
-              'Login',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400),
-            ),
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
